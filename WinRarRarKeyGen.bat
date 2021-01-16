87afcc613ac1b017a9f8909835e1fcbbd27fffa2
crack4u454 <73942395+crack4u454@users.noreply.github.com>
2021-01-16 17:15:40 +0530

    WinRarRarKeyGen

---
+++ WinRarRarKeyGen.bat
@@ -0,0 +1,24 @@
+@echo off
+(
+echo RAR s data
+echo Crack4u
+echo CRACK4U-WINRARRR-RAR_DKDK
+echo UID=431b7f545ebc1d5cba09
+echo 6412212250ba09509d652944179e7434a9b612cb339920f8847dbe
+echo 5a26b88a1814dedce0f060ddc3fb7e5c86f0be06764bffdd4be56d
+echo 72cd3c06c8563661c61e1f697465b64f5bcf7468bc25a6c9bf9334
+echo 9536a9216f463d96754ec2d1dce11dcdcb60c1a35d06071eb07f03
+echo c7b7430b9e38cbc3468840c4361211891932ad1025de0f20b3bc6f
+echo 1ea2f8ff7f35e563844f4cfb68c1a1ed8d7815b4af44ab2960dded
+echo 4e677963c76209de4dbd87633344a6876b02798b10251582245245
+) > rarreg.key
+echo Rar Register Key Generated;
+pause
+start winrar
+echo Drag rareg.key to winRar
+set /p INPUT=Is Activated?
+pause
+If %INPUT%=="yes" echo Congrats Activated 
+If %INPUT%=="no" echo You Failed 
+
+pause
